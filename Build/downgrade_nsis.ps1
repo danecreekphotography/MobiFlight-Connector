ef373a530b55d7b31acccc5ed376b86dc04d7fae
Neil <neile@live.com>
2023-12-06 17:43:35 -0800

    More cleanup

--- Build/downgrade_nsis.ps1
+++ Build/downgrade_nsis.ps1
@@ -5,8 +5,10 @@
 
 $NsisVersion = "3.04"
 Invoke-WebRequest "https://netcologne.dl.sourceforge.net/project/nsis/NSIS%203/${NsisVersion}/nsis-${NsisVersion}-setup.exe" -OutFile "C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe"
-Invoke-Expression "& C:\WINDOWS\Temp\nsis-3.04-setup.exe /S"
+Invoke-Expression "& C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe /S"
 
 $NsisPath = "${env:ProgramFiles(x86)}\NSIS\"
 Add-MachinePathItem $NsisPath
 $env:Path = Get-MachinePath
+
+makensis.exe /VERSION
