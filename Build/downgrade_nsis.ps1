f4be74928e317e78c237c48fdb58500780d01992
Neil <neile@live.com>
2023-12-06 17:34:58 -0800

    Try via script

--- Build/downgrade_nsis.ps1
+++ Build/downgrade_nsis.ps1
@@ -3,8 +3,8 @@
 ##  Desc:  Install NSIS
 ################################################################################
 
-$NsisVersion = "3.04"
-Install-Binary -Url "https://downloads.sourceforge.net/project/nsis/NSIS%203/${NsisVersion}/nsis-${NsisVersion}-setup.exe" -Name  "nsis-${NsisVersion}-setup.exe" -ArgumentList ('/S')
+Invoke-WebRequest "https://netcologne.dl.sourceforge.net/project/nsis/NSIS%203/3.04/nsis-3.04-setup.exe" -OutFile "C:\WINDOWS\Temp\nsis-3.04-setup.exe"
+Invoke-Expression "& C:\WINDOWS\Temp\nsis-3.04-setup.exe /S"
 
 $NsisPath = "${env:ProgramFiles(x86)}\NSIS\"
 Add-MachinePathItem $NsisPath
