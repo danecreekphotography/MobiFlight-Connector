5534f19583da63d9c3caadd208a15e9f9b3b7c96
Neil <neile@live.com>
2023-12-06 17:47:35 -0800

    Capture and log output

--- Build/downgrade_nsis.ps1
+++ Build/downgrade_nsis.ps1
@@ -4,11 +4,22 @@
 ################################################################################
 
 $NsisVersion = "3.04"
-Invoke-WebRequest "https://netcologne.dl.sourceforge.net/project/nsis/NSIS%203/${NsisVersion}/nsis-${NsisVersion}-setup.exe" -OutFile "C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe"
-Invoke-Expression "& C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe /S"
+$downloadUrl = "https://netcologne.dl.sourceforge.net/project/nsis/NSIS%203/${NsisVersion}/nsis-${NsisVersion}-setup.exe"
+$downloadPath = "C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe"
+
+Write-Host "Downloading NSIS version $NsisVersion"
+$response = Invoke-WebRequest $downloadUrl -OutFile $downloadPath -PassThru
+Write-Host "Downloaded $($response.BaseResponse.ContentLength) bytes"
+
+Write-Host "Installing NSIS version $NsisVersion"
+$installOutput = Invoke-Expression "& $downloadPath /S 2>&1"
+Write-Host "Install Output: $installOutput"
 
 $NsisPath = "${env:ProgramFiles(x86)}\NSIS\"
+Write-Host "Adding NSIS to the PATH"
 Add-MachinePathItem $NsisPath
 $env:Path = Get-MachinePath
 
-makensis.exe /VERSION
+Write-Host "Checking NSIS version"
+$versionOutput = makensis.exe /VERSION
+Write-Host "NSIS Version: $versionOutput"
