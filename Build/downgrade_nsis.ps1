9969fb7eb552e7d49270ea1fc910273f6839f09b
Neil <neile@live.com>
2023-12-06 17:58:15 -0800

    Try a different way again

--- Build/downgrade_nsis.ps1
+++ Build/downgrade_nsis.ps1
@@ -5,7 +5,7 @@
 
 $NsisVersion = "3.04"
 Invoke-WebRequest "https://netcologne.dl.sourceforge.net/project/nsis/NSIS%203/${NsisVersion}/nsis-${NsisVersion}-setup.exe" -OutFile "C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe"
-Invoke-Expression "& C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe /S"
+Start-Process -Wait -FilePath "C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe" -ArgumentList "/S"
 
 $NsisPath = "${env:ProgramFiles(x86)}\NSIS\"
 Add-MachinePathItem $NsisPath
